656270abc69055d20b1be464901d739db1bbe85d
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-21 14:50:43 +0800

    [task 5.2] implemented logging out

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Notification from './components/Notification'
+import LoginInformation from './components/LoginInformation'
 import LoginForm from './components/LoginForm'
 import BlogList from './components/BlogList'
 import blogService from './services/blogs'
@@ -9,11 +10,34 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
   
+  /* Load blogs on startup */
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const loadInitialBlogs = async () => {
+      setIsLoading(true)
+      setIsError(false)
+      try {
+        const blogs = await blogService.getAll()
+        setBlogs(blogs)
+      } catch (error) {
+        notify(`Error fetching blogs: ${error.message}`, 'error')
+        setIsError(true)
+      }
+      setIsLoading(false)
+    }
+    loadInitialBlogs()
+  }, [])
+
+  /* Check for logged in user on startup */
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
   
   const notify = (message, type='info') => {
@@ -27,18 +51,27 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password })
       setUser(user)
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       notify(`login as ${user.name} successful`, 'success')
       return true
-    } catch {
-      notify('wrong credentials', 'error')
+    } catch (error) {
+      notify(`Unsuccessful login: ${error.message}`, 'error')
       return false
     }
   }
+
+  if (isError)
+    return (<div>Error loading blogs</div>)
+  if (isLoading)
+    return (<div>Loading blogs...</div>)
 
   return (
     <div>
       <Notification notification={notification}/>
       {!user && <LoginForm handleLogin={handleLogin}/>}
+      {user && <h2>blogs</h2>}
+      {user && <LoginInformation user={user}/>}
       {user && <BlogList blogs={blogs} user={user} />}
     </div>
   )
--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -3,10 +3,6 @@ import Blog from './Blog'
 const BlogList = ({ blogs, user }) => {
     return (
       <div>
-        <h2>blogs</h2>
-        <div>
-          {user.name} logged in
-        </div>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
