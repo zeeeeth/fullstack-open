dfa33a2becb723c3b98b6f9ca86a4b1809229c0b
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-23 00:10:59 +0800

    [task 5.17] configure playwright, test to ensure that application displays login form by default

---
+++ part5/bloglist-frontend/playwright/tests/blog_app.spec.js
@@ -0,0 +1,11 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('log in to application')).toBeVisible()
+  })
+})
