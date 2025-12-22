6984091578cf7f9563c97320aa2ef6a464d0c0eb
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-23 02:58:02 +0800

    [tasks 5.17-5.19] tests

--- part5/bloglist-frontend/playwright/tests/blog_app.spec.js
+++ part5/bloglist-frontend/playwright/tests/blog_app.spec.js
@@ -1,11 +1,47 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { createUser, attemptLogin, createBlog } = require('./helper')
 
-describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await createUser(request, 'testuser', 'Test User', 'password')
     await page.goto('/')
-  })
+})
 
-  test('Login form is shown', async ({ page }) => {
+describe('Blog app by default', () => {
+  test('shows login form', async ({ page }) => {
     await expect(page.getByText('log in to application')).toBeVisible()
   })
+})
+
+describe('Blog app with one user', () => {
+    test('login fails with wrong password', async ({ page }) => {
+      await attemptLogin(page, 'testuser', 'wrongpassword')
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
+    
+    test('login succeeds with correct password', async ({ page }) => {
+      await attemptLogin(page, 'testuser', 'password')
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+})
+
+describe('Blog app with logged in user', () => {
+    beforeEach(async ({ page, request }) => {
+        await attemptLogin(page, 'testuser', 'password')
+        await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('can create a blog', async ({ page }) => {
+        await createBlog({ page, title: 'Test Blog Title', author: 'Test Author', url: 'http://testblog.com' })
+        await expect(page.getByText('a new blog "Test Blog Title" by Test Author added')).toBeVisible()
+    })
+
+    test('which creates a blog, created blog is visible in the list', async ({ page }) => {
+        await createBlog({ page, title: 'Test Blog Title', author: 'Test Author', url: 'http://testblog.com' })
+        await createBlog({ page, title: 'Another Blog', author: 'Another Author', url: 'http://anotherblog.com' })
+        await createBlog({ page, title: 'Third Blog', author: 'Third Author', url: 'http://thirdblog.com' })
+        await expect(page.locator('span', { hasText: 'Test Blog Title' })).toBeVisible()
+        await expect(page.locator('span', { hasText: 'Another Blog' })).toBeVisible()
+        await expect(page.locator('span', { hasText: 'Third Blog' })).toBeVisible()
+    })
 })
---
+++ part5/bloglist-frontend/playwright/tests/helper.js
@@ -0,0 +1,25 @@
+const createUser = async (request, username, name, password) => {
+    await request.post('/api/users', {
+    data: {
+        username,
+        name,
+        password
+    }
+  })
+}
+
+const createBlog = async ({ page, title, author, url }) => {
+    await page.getByRole('button', { name: 'create new blog' }).click()
+    await page.getByLabel('title:').fill(title)
+    await page.getByLabel('author:').fill(author)
+    await page.getByLabel('url:').fill(url)
+    await page.getByRole('button', { name: 'create' }).click()
+}
+
+const attemptLogin = async (page, username, password) => {
+    await page.getByLabel('username').fill(username)
+    await page.getByLabel('password').fill(password)
+    await page.getByRole('button', { name: 'login' }).click()
+}
+
+export { createUser, attemptLogin, createBlog }
