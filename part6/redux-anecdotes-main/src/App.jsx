c9a229a5afc532fd96782bfafc0d2f5ef3ad1070
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-24 06:52:54 +0300

    [tasks 6.6-6.8] removal of unnecessary code from App.jsx

--- part6/redux-anecdotes-main/src/App.jsx
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -1,10 +1,7 @@
-import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const dispatch = useDispatch()
-
   return (
     <div>
       <h2>Anecdotes</h2>
