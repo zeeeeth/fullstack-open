28af9b252fc9e2d77e0682995962aca787e34d7e
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-24 03:18:16 +0300

    [task 6.2] functionality of application

--- part6/unicafe-redux-main/src/main.jsx
+++ part6/unicafe-redux-main/src/main.jsx
@@ -5,15 +5,32 @@ import counterReducer from './reducers/counterReducer'
 const store = createStore(counterReducer)
 
 const App = () => {
+
+  const good = () => {
+    store.dispatch({ type: 'GOOD' })
+  }
+
+  const bad = () => {
+    store.dispatch({ type: 'BAD' })
+  }
+
+  const ok = () => {
+    store.dispatch({ type: 'OK' })
+  }
+
+  const reset = () => {
+    store.dispatch({ type: 'RESET' })
+  }
+
   return (
     <div>
-      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={good} >good</button>
+      <button onClick={ok}   >ok</button>
+      <button onClick={bad}  >bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
