ef3fb9b6acebd934b0c72ac28b0d311c0d5039a8
Ong, Seth <seth.ong24@imperial.ac.uk>
2025-12-31 13:54:29 +0000

    [task 7.9] implement prettier

---
+++ part7/bloglist-frontend/src/App.jsx
@@ -0,0 +1,115 @@
+import { useState, useEffect, createRef } from 'react'
+
+import blogService from './services/blogs'
+import loginService from './services/login'
+import storage from './services/storage'
+import Login from './components/Login'
+import Blog from './components/Blog'
+import NewBlog from './components/NewBlog'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  useEffect(() => {
+    const user = storage.loadUser()
+    if (user) {
+      setUser(user)
+    }
+  }, [])
+
+  const blogFormRef = createRef()
+
+  const notify = (message, type = 'success') => {
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
+
+  const handleLogin = async (credentials) => {
+    try {
+      const user = await loginService.login(credentials)
+      setUser(user)
+      storage.saveUser(user)
+      notify(`Welcome back, ${user.name}`)
+    } catch (error) {
+      notify('Wrong credentials', 'error')
+    }
+  }
+
+  const handleCreate = async (blog) => {
+    const newBlog = await blogService.create(blog)
+    setBlogs(blogs.concat(newBlog))
+    notify(`Blog created: ${newBlog.title}, ${newBlog.author}`)
+    blogFormRef.current.toggleVisibility()
+  }
+
+  const handleVote = async (blog) => {
+    console.log('updating', blog)
+    const updatedBlog = await blogService.update(blog.id, {
+      ...blog,
+      likes: blog.likes + 1,
+    })
+
+    notify(`You liked ${updatedBlog.title} by ${updatedBlog.author}`)
+    setBlogs(blogs.map((b) => (b.id === blog.id ? updatedBlog : b)))
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    storage.removeUser()
+    notify(`Bye, ${user.name}!`)
+  }
+
+  const handleDelete = async (blog) => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      await blogService.remove(blog.id)
+      setBlogs(blogs.filter((b) => b.id !== blog.id))
+      notify(`Blog ${blog.title}, by ${blog.author} removed`)
+    }
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <Notification notification={notification} />
+        <Login doLogin={handleLogin} />
+      </div>
+    )
+  }
+
+  const byLikes = (a, b) => b.likes - a.likes
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification notification={notification} />
+      <div>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </div>
+      <Togglable buttonLabel="create new blog" ref={blogFormRef}>
+        <NewBlog doCreate={handleCreate} />
+      </Togglable>
+      {blogs.sort(byLikes).map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          handleVote={handleVote}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ part7/bloglist-frontend/src/components/Blog.test.jsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { describe, test } from 'vitest'
+
+import Blog from './Blog'
+
+describe('Blog', () => {
+  const blog = {
+    title: 'Testing the testing',
+    url: 'http://example.com',
+    author: 'Ted Tester',
+    likes: 10,
+  }
+
+  test('renders only tile and author by default', () => {
+    render(<Blog blog={blog} handleVote={vi.fn()} handleDelete={vi.fn()} />)
+
+    expect(
+      screen.getByText('Testing the testing', { exact: false }),
+    ).toBeDefined()
+    expect(
+      screen.queryByText('http://example.com', { exact: false }),
+    ).toBeNull()
+  })
+
+  test('renders url and likes after clicking view', async () => {
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} handleVote={vi.fn()} handleDelete={vi.fn()} />)
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(
+      screen.getByText('http://example.com', { exact: false }),
+    ).toBeDefined()
+    expect(screen.getByText('likes 10', { exact: false })).toBeDefined()
+  })
+
+  test('clicking like twice calls event handler twice', async () => {
+    const handleVote = vi.fn()
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} handleVote={handleVote} handleDelete={vi.fn()} />)
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(handleVote.mock.calls).toHaveLength(2)
+  })
+})
